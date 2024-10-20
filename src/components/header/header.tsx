433e67a91c9e64ac09768288172608e45e99ceaf
tinatin92 <tinatygordadze@gmail.com>
2024-10-20 19:50:36 +0400

    language selector in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,48 @@
-import React from "react";
-import { NavLink, NavLinkRenderProps } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import { NavLink, NavLinkRenderProps, useParams, useNavigate } from "react-router-dom";
 import { Container } from "../UI/container";
 import headerLogo from "@/assets/logo.svg";
 import searchIcon from "@/assets/Search.svg";
 import classes from "./header.module.css";
 
+const navData = {
+  en: {
+    about: "About AllGlobe",
+    contact: "Contact",
+  },
+  ka: {
+    about: "ჩვენს შესახებ",
+    contact: "კონტაქტი",
+  },
+};
+
 export const Header: React.FC = () => {
+  const { lang } = useParams();  
+  const navigate = useNavigate();
+
+
+  const [content, setContent] = useState(navData.ka);
+
+  
+  useEffect(() => {
+    if (lang && navData[lang]) {
+      
+      setContent(navData[lang]);  
+    } else {
+      
+      setContent(navData.ka);
+    }
+  }, [lang]);
+
+
+  const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedLang = e.target.value;
+    navigate(`/${selectedLang}`); 
+  };
+
   const handleActiveNav = (props: NavLinkRenderProps) => {
     const { isActive } = props;
-
-    if (isActive) {
-      return classes["active"];
-    }
+    return isActive ? classes["active"] : undefined;
   };
 
   return (
@@ -26,15 +56,16 @@ export const Header: React.FC = () => {
           </div>
           <div className={classes.navigation}>
             <nav>
-              <NavLink to="/" className={handleActiveNav}>
-                Home
-              </NavLink>
               <NavLink to="about" className={handleActiveNav}>
-                About AllGlobe
+                {content.about}
               </NavLink>
               <NavLink to="contact" className={handleActiveNav}>
-                Contact
+                {content.contact}
               </NavLink>
+              <select value={lang} name="lang" id="lang" onChange={handleLanguageChange}>
+                <option value="en">ENG</option>
+                <option value="ka">GEO</option>
+              </select>
             </nav>
           </div>
           <div className={classes.search}>
